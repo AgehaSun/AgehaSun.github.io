0f43cb9715ad66f200e282feb3426a08d20a0bc9
Apocrita <apocrita@foxmail.com>
2021-06-19 13:35:40 +0800

    scrollbar

--- scripts/test.js
+++
@@ -1,7 +0,0 @@
-var item = document.getElementsByTagName('main')[0];
-
-window.addEventListener('wheel', function(e) {
-
-  if (e.deltaY > 0) item.scrollLeft += 100;
-  else item.scrollLeft -= 100;
-});
