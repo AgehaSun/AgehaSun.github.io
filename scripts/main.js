8b3cf34574498b6caea75099ca945b90f3dae920
Apocrita <apocrita@foxmail.com>
2021-07-06 16:12:15 +0800

    th

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+
 var item = document.querySelector('.wrapper')
 
 window.addEventListener('wheel', function(e) {
@@ -5,8 +6,3 @@ window.addEventListener('wheel', function(e) {
   if (e.deltaY > 0) item.scrollLeft += 100;
   else item.scrollLeft -= 100;
 });
-
-const cursor = document.querySelector('.cursor')
-document.addEventListener('mousemove',e => {
-  cursor.setAttribute("style","top:" + (e.pageY - 20) +"px;left:" +(e.pageX - 20)+"px")
-})
