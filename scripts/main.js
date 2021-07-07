d0ca55b8c460fd18a80d39dcb6bd46444cfe005f
Apocrita <apocrita@foxmail.com>
2021-07-07 15:31:25 +0800

    menu

--- scripts/main.js
+++ scripts/main.js
@@ -6,3 +6,15 @@ window.addEventListener('wheel', function(e) {
   if (e.deltaY > 0) item.scrollLeft += 100;
   else item.scrollLeft -= 100;
 });
+
+function myFunction() {
+  var navbar = document.getElementById("nav-bar");
+  var y = document.getElementById("nav-menu")
+  if (navbar.style.display === "flex") {
+    y.style.opacity= 0.5;
+    navbar.style.display = "none";
+  } else {
+    navbar.style.display = "flex";
+    y.style.opacity = 1;
+  }
+}
