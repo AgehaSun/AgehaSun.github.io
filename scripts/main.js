5ef39841d6bfb3b2f76b909e5c94bc2e8e155deb
Apocrita <apocrita@foxmail.com>
2021-09-17 09:58:13 +0800

    ss

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,38 @@
-let item = document.getElementById('homeArea')
+//横向滚动
+let item = document.getElementById('projectsArea');
 
-window.addEventListener('wheel', function (e) {
+item.addEventListener('wheel', function (e) {
 
     if (e.deltaY > 0) item.scrollLeft += 100;
     else item.scrollLeft -= 100;
 });
 
-function myFunction() {
+
+//导航栏折叠
+let menuButton = document.getElementById("nav-menu");
+
+menuButton.addEventListener("click",function (){
+    menuShow();
+})
+
+function menuShow() {
     let navbar = document.getElementById("nav-bar");
     let record = document.getElementById("recordinfo");
-    let y = document.getElementById("nav-menu")
+    let y = document.getElementById("nav-menu");
 
     if (navbar.style.display === "flex") {
-        y.style.opacity = 0.5;
+        y.style.opacity = "0.5";
         navbar.style.display = "none";
         record.style.display = "none"
     } else {
         navbar.style.display = "flex";
         record.style.display = "flex";
-        y.style.opacity = 1;
+        y.style.opacity = "1";
     }
 }
 
 
+//导航栏链接
 let homeButton = document.getElementById("homeButton");
 let projectButton = document.getElementById("projectButton");
 let noteButton = document.getElementById("noteButton");
@@ -30,30 +40,28 @@ let othersButton = document.getElementById("othersButton");
 let aboutButton = document.getElementById("aboutButton");
 
 
-
-homeButton.addEventListener('click',function (){
-    openPage("homeArea",homeButton);
+homeButton.addEventListener('click', function () {
+    openPage("homeArea", homeButton);
 })
 
-projectButton.addEventListener('click',function (){
-    openPage("projectsArea",projectButton);
+projectButton.addEventListener('click', function () {
+    openPage("projectsArea", projectButton);
 })
 
-noteButton.addEventListener('click',function (){
-    openPage("notesArea",noteButton);
+noteButton.addEventListener('click', function () {
+    openPage("notesArea", noteButton);
 })
 
-othersButton.addEventListener('click',function (){
-    openPage("othersArea",othersButton);
+othersButton.addEventListener('click', function () {
+    openPage("othersArea", othersButton);
 })
 
-aboutButton.addEventListener('click',function (){
-    openPage("aboutArea",aboutButton);
+aboutButton.addEventListener('click', function () {
+    openPage("aboutArea", aboutButton);
 })
 
 
-
-function openPage(tabName,activeButton) {
+function openPage(tabName, activeButton) {
     let i, tabcontent, tablinks;
 
     tabcontent = document.getElementsByClassName("tabContent")
