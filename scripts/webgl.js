74a3786e9423c44baafd21bb7fa91e9c99d4f514
AgehaSun <agehadriftage@gmail.com>
2023-09-28 17:43:29 +0800

    ss

--- scripts/webgl.js
+++ scripts/webgl.js
@@ -2,8 +2,14 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
-let camera, scene, renderer;
-const webglContainer = document.getElementById("webgl") ;
+
+const canvas = document.getElementById("webgl");
+const camera = new THREE.PerspectiveCamera(60, canvas.offsetWidth / canvas.offsetHeight, 0.1, 100);
+const scene = new THREE.Scene();
+const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true, canvas: canvas });
+const loader = new GLTFLoader();
+const controls = new OrbitControls(camera, renderer.domElement);
+
 
 
 Init();
@@ -13,68 +19,46 @@ Render();
 function Init()
 {
     //camera
-    camera = new THREE.PerspectiveCamera( 60,  webglContainer.offsetWidth/ webglContainer.offsetHeight, 0.1, 100 );
-    camera.position.set( 0, 0.9, 4.5);
-
-
-
-    //scene
-    scene = new THREE.Scene();
-    // scene.background = new THREE.Color(0x000000);
-
+    camera.position.set(0, 0.9, 4.5);
 
     //loader
-    const loader = new GLTFLoader();
-    loader.load( 'webgl/forest_house.glb', function ( gltf ) {
-
-        scene.add( gltf.scene );
-        gltf.scene.position.set(3,0,-1);
-        gltf.scene.scale.set(20,20,20);
+    loader.load('webgl/forest_house.glb', function (gltf)
+    {
+        scene.add(gltf.scene);
+        gltf.scene.position.set(3, 0, -1);
+        gltf.scene.scale.set(20, 20, 20);
         Render();
 
-    }, undefined, function ( error ) {
+    }, undefined, function (error)
+    {
+        console.error(error);
+    });
 
-        console.error( error );
 
-    } );
-
-
-    //renderer
-    renderer = new THREE.WebGLRenderer(
-        {
-            antialias: true ,
-            alpha: true,
-        });
-
-    webglContainer.appendChild(renderer.domElement);
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize(webglContainer.offsetWidth, webglContainer.offsetHeight);
-
+    //webglContainer.appendChild(renderer.domElement);
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(canvas.offsetWidth, canvas.offsetHeight);
 
     //orbit Controller
-    const controls = new OrbitControls(camera, renderer.domElement);
-    controls.addEventListener( 'change', Render );
-    controls.target.set( 0, 2, 0 );
+    controls.addEventListener('change', Render);
+    controls.target.set(0, 2, 0);
     controls.update();
 
-    window.addEventListener( 'resize', OnWindowResize );
-
+    window.addEventListener('resize', OnWindowResize);
 
 }
 
 
 function OnWindowResize()
 {
-    camera.aspect = webglContainer.offsetWidth/ webglContainer.offsetHeight;
+    camera.aspect = canvas.offsetWidth / canvas.offsetHeight;
     camera.updateProjectionMatrix();
-
-    renderer.setSize(webglContainer.offsetWidth,webglContainer.offsetHeight);
+    renderer.setSize(canvas.offsetWidth, canvas.offsetHeight);
     Render();
 }
 
 
 function Render()
 {
-    renderer.render( scene, camera );
-
+    renderer.render(scene, camera);
 }
